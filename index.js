1ff4a308459cc9c6b264699771c6302e4b377e53
Marcelus <keyzeed@gmail.com>
2020-09-01 18:21:37 +0200

    added list ok

--- index.js
+++ index.js
@@ -2,39 +2,45 @@ class Event {
     constructor(event, date) {
         this.eventName = event;
         this.date = date;
+        this.people = [];
     }
+    addPerson(name){
+        this.people.push(name);
+        document.querySelector('.person__container').insertAdjacentHTML("beforeend", `
+        ${name} </br>`);
+        console.log(this.people.length);
+        document.querySelector('#total__people').innerHTML = `${this.people.length}`;
+    }
+
+    
 }
- 
-const eventController = ()=>{
+let newEvent;
+let totalPeople;
 
-};
-
-const UIController = ()=>{
-     return {
-         
-     }  
-};
-
-
-let getEventInfo = (function(eventController,UIController){
+let getEventInfo = (function(){
     let btn = document.getElementById('event__btn');
     btn.addEventListener('click', function(){
         let eventName = document.getElementById('name__input').value;
-        let eventDate = document.getElementById('date__input').value;
-        let newEvent = new Event(eventName, eventDate);
+        let eventDate = document.getElementById('date__input').value.split('-').reverse().toString().replace(/,/g,"/");
+        newEvent = new Event(eventName, eventDate);
         console.log(newEvent);
         const insertEvent = ()=> {
-           document.querySelector('.box__middle').insertAdjacentHTML("afterbegin", `
-            <div id="event__name">${eventName} ${eventDate}
-            </br>Total people: 00
-            </br>Total expenses: <span class=expense>00€</span>
-            </div>
-            `
-           );
+          document.querySelector('#event__name').innerHTML= `${eventName} (${eventDate})
+          </br>Total people: <span id="total__people">0</span>
+          </br>Total expenses: <span class="expense">00€</span>
+          `;
         }
         insertEvent();
-        console.log(typeof(eventDate))
-
     });
+})();
 
-})(eventController,UIController);
+getPersonInfo = () => {
+    let btn = document.getElementById('person__btn');
+    btn.addEventListener('click', function(){
+        let personName = document.getElementById('person__name').value;
+        newEvent.addPerson(personName);
+        console.log(newEvent);
+
+    })
+};
+getPersonInfo();
